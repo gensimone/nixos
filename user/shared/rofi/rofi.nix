b5aa7f18f3d988e6fef73dae07378daec9c17e1a
Simone Gentili <gensimone.git@gmail.com>
2025-05-01 21:38:58 +0200

    Update rofi.nix

--- user/shared/rofi/rofi.nix
+++ user/shared/rofi/rofi.nix
@@ -2,7 +2,7 @@
 
 {
   home.packages = with pkgs; [
-    rofi-wayland         # Window switcher, run dialog and dmenu replacement for Wayland
+    rofi-wayland # Window switcher, run dialog and dmenu replacement for Wayland
   ];
 
   xdg.configFile."rofi/config.rasi".source = config.lib.file.mkOutOfStoreSymlink /home/simone/.config/nixos/user/shared/rofi/config.rasi;
