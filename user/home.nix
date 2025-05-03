49767bdcfaf35d84a19c616a7f78e05af668a359
Simone Gentili <gensimone.git@gmail.com>
2025-05-03 18:47:46 +0200

    Update

--- user/home.nix
+++ user/home.nix
@@ -21,17 +21,26 @@
   home.file = {};
   home.sessionVariables = {};
 
-  xdg.userDirs = {
-    enable = true;
-    extraConfig = {
-      XDG_MUSIC_DIR = "${config.home.homeDirectory}/music";
-      XDG_DOCUMENTS_DIR = "${config.home.homeDirectory}/documents";
-      XDG_DOWNLOAD_DIR = "${config.home.homeDirectory}/downloads";
-      XDG_DESKTOP_DIR = "${config.home.homeDirectory}/desktop";
-      XDG_PICTURES_DIR = "${config.home.homeDirectory}/pictures";
-      XDG_PUBLICSHARE_DIR = "${config.home.homeDirectory}/public";
-      XDG_TEMPLATES_DIR = "${config.home.homeDirectory}/templates";
-      XDG_VIDEOS_DIR = "${config.home.homeDirectory}/videos";
+  xdg = {
+    userDirs = {
+      enable = true;
+      extraConfig = {
+        XDG_MUSIC_DIR = "${config.home.homeDirectory}/music";
+        XDG_DOCUMENTS_DIR = "${config.home.homeDirectory}/documents";
+        XDG_DOWNLOAD_DIR = "${config.home.homeDirectory}/downloads";
+        XDG_DESKTOP_DIR = "${config.home.homeDirectory}/desktop";
+        XDG_PICTURES_DIR = "${config.home.homeDirectory}/pictures";
+        XDG_PUBLICSHARE_DIR = "${config.home.homeDirectory}/public";
+        XDG_TEMPLATES_DIR = "${config.home.homeDirectory}/templates";
+        XDG_VIDEOS_DIR = "${config.home.homeDirectory}/videos";
+      };
+    };
+
+    mimeApps.defaultApplications = {
+      "text/plain" = [ "neovim.desktop" ];
+      "application/pdf" = [ "zathura.desktop" ];
+      "image/*" = [ "swayimg.desktop" ];
+      "video/*" = [ "mpv.desktop" ];
     };
   };
 
