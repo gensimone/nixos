f067c29dfa8c153ef227fd9038a8263122e670b7
Simone Gentili <gensimone.git@gmail.com>
2025-05-08 11:29:16 +0200

    Update

--- home/shared/rofi/scripts/wallset.sh
+++ home/shared/rofi/scripts/wallset.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-swww img $(for a in ~/pictures/wallpapers/* ; do echo -en "$a\0icon\x1f$a\n" ; done | rofi -config ~/.config/rofi/wallset.rasi -dmenu)
+result=$(for a in ~/pictures/wallpapers/* ; do echo -en "$a\0icon\x1f$a\n" ; done | rofi -config ~/.config/rofi/wallset.rasi -dmenu)
+swww img "$result" --transition-type center --transition-fps 120 --transition-step 50
--- home/shared/wallpapers/wallpapers.nix
+++ home/shared/wallpapers/wallpapers.nix
@@ -1,1 +1,1 @@
-{ ... }: { home.file."pictures/wallpapers".source = ./wallpapers; }
+{ config, ... }: { home.file."pictures/wallpapers".source = config.lib.file.mkOutOfStoreSymlink /home/simone/.config/nixos/home/shared/wallpapers/wallpapers; }
