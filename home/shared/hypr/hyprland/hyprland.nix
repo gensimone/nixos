dd6cb983bcf6a8e23b0568e8ea75f3ed77632fd9
Simone Gentili <gensimone.git@gmail.com>
2025-05-11 16:00:20 +0200

    Update

--- home/shared/hypr/hyprland/hyprland.nix
+++ home/shared/hypr/hyprland/hyprland.nix
@@ -363,6 +363,7 @@
         "float,class:^(io.missioncenter.MissionCenter)$"
         "float,class:^(xdg-desktop-portal-gtk)$"
         "float,class:^(pulsemixer)$"
+        "float,title:^(Syncthing Tray)$"
 
         "size 620 650,class:^(clipse)$"
         "size 620 350,class:^(pulsemixer)$"
