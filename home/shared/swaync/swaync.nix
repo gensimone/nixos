05d628619b4f433b12e3737fe4af0782065a6b35
Simone Gentili <gensimone.git@gmail.com>
2025-05-12 18:52:36 +0200

    Update

--- home/shared/swaync/swaync.nix
+++ home/shared/swaync/swaync.nix
@@ -64,7 +64,7 @@
               command = "systemctl poweroff";
             }
             {
-              label = "󰜉";
+              label = "";
               command = "systemctl reboot";
             }
             {
