da45e11a654cadd89e1e130e1aa430b39e5eac5c
Simone Gentili <gensimone.git@gmail.com>
2025-05-26 16:32:21 +0200

    Update lsp.nix

--- home/shared/nvim/plugins/lsp.nix
+++ home/shared/nvim/plugins/lsp.nix
@@ -5,10 +5,57 @@
     lsp.enable = true;
     languages = {
       enableTreesitter = true;
-      assembly.enable = true;
-      clang.enable = true;
-      nix.enable = true;
-      python.enable = true;
+      clang = {
+        enable = true;
+        treesitter.enable = true;
+        # C syntax for headers. Can fix treesitter errors, see:
+        # https://www.reddit.com/r/neovim/comments/orfpcd/question_does_the_c_parser_from_nvimtreesitter/
+        cHeader = true;
+        lsp = {
+          enable = true;
+          opts = null;
+          server = "clangd";
+        };
+      };
+      assembly = {
+        enable = true;
+        lsp.enable = true;
+        treesitter.enable = true;
+      };
+      python = {
+        enable = true;
+        treesitter.enable = true;
+        lsp = {
+          enable = true;
+          server = "basedpyright";
+        };
+        format = {
+          enable = true;
+          type = "black";
+        };
+      };
+      nix = {
+        enable = true;
+        treesitter.enable = true;
+        lsp = {
+          enable = true;
+          options = null;
+          server = "nil"; # or nixd
+        };
+        format = {
+          enable = true;
+          type = "alejandra";
+        };
+        extraDiagnostics = {
+          enable = true;
+          types = [ "statix" "deadnix" ];
+        };
+      };
+      java = {
+        enable = true;
+        lsp.enable = true;
+        treesitter.enable = true;
+      };
       rust.enable = true;
     };
   };
